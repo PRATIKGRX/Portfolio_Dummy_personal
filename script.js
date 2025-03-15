c8562cc73d6658d0f298f1b2f1ae3c7634fa542e
PRATIKGRX <pratikgrx06@gmail.com>
2025-03-15 21:52:13 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ window.addEventListener("scroll",function (){
     if(window.scrollY >= heroSec.offsetHeight){
         navBar.classList.add("fixedPosition");
         navBar.classList.remove("absolute");
+        navBar.classList.remove("nav-bar");
+        navBar.classList.add("scroll-icons");
     }
     else{
         navBar.classList.remove("fixedPosition");
+        navBar.classList.remove("scroll-icons");
         navBar.classList.add("absolute");
+        navBar.classList.add("nav-bar");
     }
 });
