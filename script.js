5352f3a5dd929574b8589d4b5ad0a5601ffadd9b
PRATIKGRX <pratikgrx06@gmail.com>
2025-04-03 17:18:50 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ window.addEventListener("scroll",function (){
         navBar.classList.add("nav-bar");
     }
 });
+function showBar(){
+    document.getElementById("btn").style.display = "none";
+    const sideBar = document.getElementById("sideBarContainer");
+    sideBar.classList.remove("hidden");
+    document.getElementById("close").style.display = "block";
+}
+function closeBtn(){
+    document.getElementById("close").style.display = "none";
+    const sideBar = document.getElementById("sideBarContainer");
+    sideBar.classList.add("hidden");
+    document.getElementById("btn").style.display = "block";
+
+}
+const dropDownBtn = document.getElementById("dropdown-btn");
+const dropDown = document.getElementById("dropdown");
+const dropDownCancel = document.getElementById("dropdown-cancel");
+dropDownBtn.addEventListener('click',()=>{
+    dropDown.classList.remove("hidden");
+    document.body.style.overflow = "hidden";
+});
+dropDownCancel.addEventListener('click',()=>{
+    dropDown.classList.add("hidden");
+    document.body.style.overflow = "auto";
+});
